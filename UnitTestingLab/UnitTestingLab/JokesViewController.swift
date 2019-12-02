ab1f555d967a7613e5ad3f65ab912ce9cb30cc6a
Eric Davenport <EricDavenport@pursuit.org>
2019-12-02 16:50:49 -0500

    API's adde - View controllers + self file added -

---
+++ UnitTestingLab/UnitTestingLab/JokesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JokesViewController.swift
+//  UnitTestingLab
+//
+//  Created by Eric Davenport on 12/2/19.
+//  Copyright © 2019 Eric Davenport. All rights reserved.
+//
+
+import UIKit
+
+class JokesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UnitTestingLab/UnitTestingLab/StarWarsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestingLab
+//
+//  Created by Eric Davenport on 12/2/19.
+//  Copyright © 2019 Eric Davenport. All rights reserved.
+//
+
+import UIKit
+
+class StarWarsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
